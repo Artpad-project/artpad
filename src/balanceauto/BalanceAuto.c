4e9785dc270e53dd4f1a8dc85e414d0ff767975c
paul.lecomte <paul.lecomte@epita.fr>
2021-05-04 19:39:35 +0200

    I was made for lovin u baby

--- src/balanceauto/BalanceAuto.c
+++ src/balanceauto/BalanceAuto.c
@@ -8,6 +8,8 @@ double max(double x, double y);
 double min(double x, double y);
 double Hue(double red, double green, double blue, double cmax, double delta);
 double absolu(double x);
+int Cap(double x);
+double CapSat(double x);
 
 void BalanceAuto(Image* Bitmap)
 {
@@ -17,22 +19,21 @@ void BalanceAuto(Image* Bitmap)
 
     int nbPix = nbLine * nbCol;
 
-    int* HistoLuminance = malloc(101 * sizeof(double));
-    memset(HistoLuminance, 0, 101);
+    int* HistoLuminance = malloc(101 * sizeof(int));
+    for(int i = 0; i<101; ++i) HistoLuminance[i]=0;
 
-    int* cdf = malloc(101 * sizeof(double));
-    memset(cdf, 0, 101);
+    int* cdf = malloc(101 * sizeof(int));
+    for(int i = 0; i<101; ++i) cdf[i]=0;
 
-    int* equalized = malloc(101 * sizeof(double));
-    memset(equalized, 0, 101);
+
 
     for (int i = 0; i < nbLine; ++i) {
 
         for (int j = 0; j < nbCol; ++j) {
 
-            double nrouge = Bitmap->pixels[i][j].red / 255.0;
-            double nvert = Bitmap->pixels[i][j].green / 255.0;
-            double nbleu = Bitmap->pixels[i][j].blue / 255.0;
+            double nrouge = Bitmap->pixels[j][i].red / 255.0;
+            double nvert = Bitmap->pixels[j][i].green / 255.0;
+            double nbleu = Bitmap->pixels[j][i].blue / 255.0;
 
             double cmax = max(nbleu, max(nrouge,nvert));
             double cmin = min(nbleu, min(nvert,nrouge));
@@ -56,9 +57,9 @@ void BalanceAuto(Image* Bitmap)
 
         for (int j = 0; j < nbCol; ++j) {
 
-            double nrouge = Bitmap->pixels[i][j].red / 255.0;
-            double nvert = Bitmap->pixels[i][j].green / 255.0;
-            double nbleu = Bitmap->pixels[i][j].blue / 255.0;
+            double nrouge = Bitmap->pixels[j][i].red / 255.0;
+            double nvert = Bitmap->pixels[j][i].green / 255.0;
+            double nbleu = Bitmap->pixels[j][i].blue / 255.0;
 
             double cmax = max(nbleu, max(nrouge,nvert));
             double cmin = min(nbleu, min(nvert,nrouge));
@@ -67,12 +68,12 @@ void BalanceAuto(Image* Bitmap)
 
             double hue = delta == 0 ? 0 : Hue(nrouge,nvert,nbleu,cmax,delta) ;
             double luminance = ((cmax + cmin)/2.0)*100;
-            double saturation = delta == 0 ? 0 : delta /(1-absolu(2*luminance-1)) ;
+            double saturation = delta == 0 ? 0 : CapSat(delta /(1-absolu(2*luminance/100-1))) ;
 
-            double newluminance =  (round((cdf[(int)luminance] - cdftmin)/(double)((nbPix-cdftmin)*255.0)))/100.0;
+            double newluminance =  round( ((cdf[(int)luminance] - cdftmin) / (double)( (nbPix-cdftmin))) * 100.0 ) /100.0;
 
             double C = (1-absolu(2*newluminance-1))*saturation;
-            double X = C * (1-absolu((int)(hue/60)%2 - 1));
+            double X = C * (1-absolu(fmod((hue/60),2) - 1));
             double M = newluminance - C/2;
 
             if (hue >= 0 && hue < 60)
@@ -96,33 +97,36 @@ void BalanceAuto(Image* Bitmap)
                 nbleu = X;
             }
 
-            if (hue >= 180 && hue < 240)
+            else if (hue >= 180 && hue < 240)
             {
                 nrouge = 0;
                 nvert = X;
                 nbleu = C;
             }
 
-            if (hue >= 240 && hue < 300)
+            else if (hue >= 240 && hue < 300)
             {
                 nrouge = X;
                 nvert = 0;
                 nbleu = C;
             }
 
-            if (hue >= 300 && hue < 360)
+            else if (hue >= 300 && hue < 360)
             {
                 nrouge = C;
                 nvert = 0;
                 nbleu = X;
             }
-
-            Bitmap->pixels[i][j].red = (int)((nrouge+M) * 255);
-            Bitmap->pixels[i][j].green = (int)((nvert+M) *  255);
-            Bitmap->pixels[i][j].blue = (int)((nbleu+M) * 255);
+	    //printf("%f %f %f \n",nrouge,nvert,nbleu);
+            Bitmap->pixels[j][i].red = Cap((nrouge+M) * 255);
+            Bitmap->pixels[j][i].green = Cap((nvert+M) *  255);
+            Bitmap->pixels[j][i].blue = Cap((nbleu+M) * 255);
 
         }
     }
+
+    free(HistoLuminance);
+    free(cdf);
 
 }
 
@@ -139,8 +143,12 @@ double min(double x, double y) {
 
 double Hue(double red, double green, double blue, double cmax, double delta)
 {
-    if(red == cmax) return  60 * ((int)((green - blue)/delta) %6);
-    if(green == cmax) return 60 * (((blue - red)/delta) + 2);
+  if(red == cmax)
+    {
+      double res =  60 * (fmod(((green - blue)/delta),6));
+      return res <0 ? res +360 : res;
+    }
+   if(green == cmax) return 60 * (((blue - red)/delta) + 2);
     return 60 * (((red - green)/delta) + 4);
 }
 
@@ -148,5 +156,20 @@ double absolu(double x)
 {
     double res = x < 0 ? -x : x;
     return res;
+
+}
+
+int Cap(double x)
+{
+  double res = x < 0 ? 0 : x;
+  res = x > 255 ? 255 : x;
+  return (int)res;
+}
+
+double CapSat(double x)
+{
+  if(x>1)return 1.0;
+  if(x<-1)return -1.0;
+  return x;
 
 }
--- src/balanceauto/main.c
+++ src/balanceauto/main.c
@@ -1,9 +1,9 @@
-#include "PuissanceN.h"
+#include "BalanceAuto.h"
 
 int main()
 {
-  Image* Bitmap = load_image("possion.jpg");
-  BalanceAbsolue(Bitmap);
+  Image* Bitmap = load_image("Test/yiff.jpg");
+  BalanceAuto(Bitmap);
   save_image(Bitmap,"result","jpeg");
   free_image(Bitmap);
   return 0;
