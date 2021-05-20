7f1e151fc26f299716958f80a6e87828230bc987
Paul Lecomte <paul.lecomte@epita.fr>
2021-05-20 17:51:00 +0200

    Update main.c

--- src/detection/main.c
+++ src/detection/main.c
@@ -1,11 +1,17 @@
 #include "DetectionCoin.h"
+#include "KLT.h"
 
 int main()
 {
-  Image* Bitmap = load_image("Test/croix2.jpg");
-  Detection(Bitmap,1.0,2.5,0.006,130);
-  //save_image(Bitmap,"result","png");
-  free_image(Bitmap);
-  free(Bitmap);
+  int*  nbPoint;
+  Coord* CoordList;
+  Image* Bitmap1 = load_image("Test/croix2.jpg");
+  Image* Bitmap2 = load_image("Test/croix2.jpg");
+  CoordList = Detection(Bitmap1,1.0,2.5,0.006,130,nbPoint);
+  KLT(Bitmap1,Bitmap2,5,CoordList,nbPoint);
+  free_image(Bitmap1);
+  free_image(Bitmap2);
+  free(Bitmap1);
+  free(Bitmap2);
   return 0;
 }
